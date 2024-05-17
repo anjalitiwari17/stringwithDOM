496ddb59beb71a3e8d2ef8117d7b701829505281
anjalitiwari17 <anjalitiwari152004@gmail.com>
2024-05-17 13:51:30 +0530

    commit

--- app.js
+++ app.js
@@ -1,7 +1,5 @@
 // 2.
 
-
-
 // function check(arr) {
 //         const result = [];
 //         for (let i = 0; i < arr.length; i++) {
@@ -13,16 +11,14 @@
 //         }
 //         return result;
 //     }
-    
+
 //     const input = ["javascript", "react", "nodeJs"];
 //     const output = check(input);
-//     console.log(output);  
-    
+//     console.log(output);
+
 //     const input1 = ["java", "py", "c++"];
 //     const output1 = check(input1);
-//     console.log(output1); 
-    
-
+//     console.log(output1);
 
 // 6.
 
@@ -30,27 +26,22 @@
 //         const word = sentence.split(' ');
 //         const reverseWords = word.reverse();
 //         const reverseSentence = reverseWords.join(' ');
-        
+
 //         return reverseSentence;
 //     }
-    
+
 //     const input = "Success comes to those who work hard";
 //     const output = reverseByWord(input);
-//     console.log(output); 
-    
-
-
+//     console.log(output);
 
 // 5.
 // function reverseString(input) {
 //         return input.split("").reverse().join("");
 //     }
-    
+
 //     var input = "i am learning javascript";
 //     var reversed = reverseString(input);
 //     console.log(reversed);
-    
-
 
 // 1.
 // function capitalize(line) {
@@ -58,11 +49,38 @@
 //         for (let i = 0; i < words.length; i++) {
 //           words[i] = words[i].charAt(0).toUpperCase() + words[i].slice(1);
 //         }
-      
+
 //         return words.join(' ');
 //       }
-      
+
 //       let input = "this is javascript";
 //       let output = capitalize(input);
-//       console.log(output); 
-      
+//       console.log(output);
+
+
+
+
+// let input = document.getElementById("input");
+// let btn = document.getElementById("btn");
+// let output = document.getElementById("output");
+
+// btn.addEventListener("click", () => {
+//   let inputValue = input.value;
+//   let res = capitalize(inputValue);
+//   output.innerHTML = `<h2>${res}</h2>`;
+// });
+
+// function capitalize(str) {
+//   let s = "";
+//   for (let i = 0; i < str.length; i++) {
+//     if (i == 0 || str[i - 1] == " ") {
+//       s = s + str[i].toUpperCase();
+//     } else {
+//       s = s + str[i];
+//     }
+//   }
+//   return s;
+// }
+// let st = "this is javascript";
+// let res = capitalize(st);
+// console.log(res);
